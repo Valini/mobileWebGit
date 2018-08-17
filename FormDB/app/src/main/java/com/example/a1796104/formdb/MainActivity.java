7b0bc039d38b3bb11abb37380aa752d89f8541d8
MSI\Valini <valinipatten@yahoo.com>
2018-08-17 19:46:13 -0400

    Friday evening

--- FormDB/app/src/main/java/com/example/a1796104/formdb/MainActivity.java
+++ FormDB/app/src/main/java/com/example/a1796104/formdb/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import com.facebook.stetho.Stetho;
 
-public class MainActivity extends MyMenuActivity {
+public class MainActivity extends AppCompatActivity {
 
     EditText nameEditText;
     EditText addressEditText;
